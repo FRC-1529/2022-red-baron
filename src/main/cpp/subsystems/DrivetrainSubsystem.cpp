29f0624b30952b6dbe083b4d8880d43aad426314
Calob Humble <cshumble@protonmail.com>
2022-03-15 12:49:37 -0400

    Base command code + created drivestrain subsystem files

---
+++ src/main/cpp/subsystems/DrivetrainSubsystem.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "subsystems/DrivetrainSubsystem.h"
+
+DrivetrainSubsystem::DrivetrainSubsystem() {
+  // Implementation of subsystem constructor goes here.
+}
+
+void DrivetrainSubsystem::Periodic() {
+  // Implementation of subsystem periodic method goes here.
+}
+
+void DrivetrainSubsystem::SimulationPeriodic() {
+  // Implementation of subsystem simulation periodic method goes here.
+}
