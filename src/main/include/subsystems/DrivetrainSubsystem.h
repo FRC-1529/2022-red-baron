cb01f4e16e9a4e95681f009aa767caa0388c8479
Calob Humble <cshumble@protonmail.com>
2022-03-15 13:19:40 -0400

    Add drivetrain motors and groups

--- src/main/include/subsystems/DrivetrainSubsystem.h
+++ src/main/include/subsystems/DrivetrainSubsystem.h
@@ -5,6 +5,9 @@
 #pragma once
 
 #include <frc2/command/SubsystemBase.h>
+#include <ctre/Phoenix.h>
+#include <frc/motorcontrol/MotorControllerGroup.h>
+#include <frc/drive/DifferentialDrive.h>
 
 class DrivetrainSubsystem : public frc2::SubsystemBase {
  public:
@@ -22,6 +25,13 @@ class DrivetrainSubsystem : public frc2::SubsystemBase {
   void SimulationPeriodic() override;
 
  private:
+  ctre::phoenix::motorcontrol::can::WPI_TalonFX motor_front_left{2};
+  ctre::phoenix::motorcontrol::can::WPI_TalonFX motor_front_right{0};
+  ctre::phoenix::motorcontrol::can::WPI_TalonFX motor_back_left{4};
+  ctre::phoenix::motorcontrol::can::WPI_TalonFX motor_back_right{1};
+  frc::MotorControllerGroup motor_group_left{motor_front_left, motor_back_left};
+  frc::MotorControllerGroup motor_group_right{motor_front_right, motor_back_right};
+  frc::DifferentialDrive differential_drive{motor_group_left, motor_group_right};
   // Components (e.g. motor controllers and sensors) should generally be
   // declared private and exposed only through public methods.
 };
